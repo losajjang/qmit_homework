d8aae1872fe4d7949fefb2cfdf7273b84f59bb5a
losajjang <losajjang@naver.com>
2022-08-23 23:51:21 +0900

    feat: keypad UI 개발

--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+import Keypad from '../components/Keypad';
+import {Display} from '../elements/index';
 
 function Calculator() {
-  return <div>Calculator</div>;
+  return (
+    <Container>
+      Calculator
+      <InnerContainer>
+        <Display />
+        <Keypad />
+      </InnerContainer>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  background: #4386e7;
+  display: inline-block;
+  border-radius: 20px;
+  padding: 20px 40px;
+`;
+
+const InnerContainer = styled.div`
+  background: white;
+  padding: 5px;
+  width: 670px;
+  height: 320px;
+  border-radius: 15px;
+`;
 
 export default Calculator;
